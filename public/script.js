3f8f3fa8907095782de1ae31e21300e239f81161
Michael <michael_kim57@yahoo.com>
2020-12-11 01:13:32 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+/*
 function getInfo(id, obj) {
   $.ajax({
     url:
@@ -97,6 +98,101 @@ function getRecipe(q) {
         getInfo(res.results[i].id, item);
         getInstructions(res.results[i].id, item);
         document.getElementById("output").appendChild(item);
+      }
+    }
+  });
+}*/
+function getInfo(id) {
+  $.ajax({
+    url:
+      "https://api.spoonacular.com/recipes/" +
+      id +
+      "/information?apiKey=f7c1bb91a6834ff68b06205d141ba628",
+    method: "GET",
+    success: function (res) {
+      var temp_list = [];
+      for (i in res.extendedIngredients) {
+        var entry = res.extendedIngredients[i]["name"];
+        temp_list.push(entry);
+      }
+      return temp_list;
+    }
+  });
+}
+
+function formRecipe(form) {
+  $.ajax({
+    method: "POST",
+    url:
+      "https://api.spoonacular.com/recipes/visualizeRecipe?apiKey=f7c1bb91a6834ff68b06205d141ba628",
+    async: true,
+    crossDomain: true,
+    headers: {
+      "content-type": "multipart/form-data"
+    },
+    contentType: false,
+    mimeType: "multipart/form-data",
+    data: form,
+    success: function (res) {
+      console.log("before");
+      console.log(res[0]);
+      //document.getElementById("output").appendChild(res)
+      console.log("after");
+    }
+  });
+}
+
+function getInstructions(id, obj) {
+  $.ajax({
+    url:
+      "https://api.spoonacular.com/recipes/" +
+      id +
+      "/analyzedInstructions?apiKey=f7c1bb91a6834ff68b06205d141ba628",
+    method: "GET",
+    success: function (res) {
+      var list = [];
+      console.log(res[0]["steps"]);
+      for (var i = 0; i < res[0]["steps"].length; i++) {
+        var step = res[0]["steps"][i]["step"];
+        console.log(step);
+        list.push(step);
+      }
+      console.log(list);
+      return list;
+    }
+  });
+}
+
+function removePrevSearch(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function getRecipe(q) {
+  $.ajax({
+    url:
+      "https://api.spoonacular.com/recipes/search?apiKey=f7c1bb91a6834ff68b06205d141ba628&number=1&query=" +
+      q,
+    method: "GET",
+    success: function (res) {
+      //removePrevSearch(document.getElementById("output"));
+      for (var i = 0; i < res.results.length; i++) {
+        var formData = new FormData();
+        formData.append("title", res.results[i].title);
+        formData.append("image", res.results[i].image);
+        formData.append("ingredients", getInfo(res.results[i].id));
+        formData.append("instructions", getInstructions(res.results[i].id));
+        formData.append("Cook Time", res.results[i].readyInMinutes);
+        formData.append("servings",res.results[i].servings);
+        formData.append("mask","ellipseMask");
+        formData.append("backgroundImage","background1");
+        formData.append("author",res.results[i].sourceName);
+        formData.append("backgroundColor","#ffffff");
+        formData.append("fontColor","#333333")
+        formData.append("source","spoonacular.com")
+
+        formRecipe(formData);
       }
     }
   });
